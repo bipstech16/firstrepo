41a5cc68dd7d618c09427e2214e968210e622b2e
bipstech16 <168907429+bipstech16@users.noreply.github.com>
2025-08-02 21:45:44 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.26"
+    }
+    
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "15a5294d-bf43-4301-914e-3af5e2295b0b"
+
+}
+
+
+
+resource "azurerm_resource_group" "pagl" {
+  name     = "testing"
+  location = "East US"
+}
